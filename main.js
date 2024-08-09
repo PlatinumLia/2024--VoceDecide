3f482008278cdb2471fd520289ed720ad92d506d
PlatinumLia <134401063+PlatinumLia@users.noreply.github.com>
2024-08-09 10:38:17 -0300

    index,style e main
    Começo do código JavaScript

---
+++ main.js
@@ -0,0 +1,22 @@
+const caixaPrincipal = document.querySelector('.caixa-principal');
+const caixaPerguntas = document.querySelector('.caixa-perguntas');
+const caixaAlternativa = document.querySelector('.caixa-alternativa');
+const caixaResultado = document.querySelector('.caixa-resultado');
+const textoResultado = document.querySelector('.texto-resultao');
+
+const perguntas = [
+    {
+        enunciado: "texto01",
+        alternativas: [
+            "Alternativa01",
+            "Alternativa02"
+        ]    
+    },
+    {
+        enunciado: "Pergunta 2",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    }
+]
