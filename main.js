055886d7f1a6b1d58a89348d80b74235959e2eea
PlatinumLia <134401063+PlatinumLia@users.noreply.github.com>
2024-08-13 11:25:38 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -18,5 +18,37 @@ const perguntas = [
             "Alternativa 1",
             "Alternativa 2"
         ]
+    },
+    {
+        enunciado: "Pergunta 3",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Pergunta 4",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Pergunta 5",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
     }
 ]
+
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta() 
+    {
+        perguntaAtual = perguntas[atual];
+        caixaPerguntas.textContent = perguntaAtual.enunciado;
+    }
+
+    mostraPergunta();
