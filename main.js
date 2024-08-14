b99c4203257e00f370777e5eb27487d321eef5c7
PlatinumLia <134401063+PlatinumLia@users.noreply.github.com>
2024-08-14 07:50:35 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -49,6 +49,17 @@ function mostraPergunta()
     {
         perguntaAtual = perguntas[atual];
         caixaPerguntas.textContent = perguntaAtual.enunciado;
+        mostraAlternativas();
+    }
+
+    function mostraAlternativas()
+    {
+        for (const alternativa of perguntaAtual.alternativas)
+        {
+            const botaoAlternativas = document.createElement("button");
+            botaoAlternativas.textContent = alternativa;
+            caixaAlternativa.appendChild(botaoAlternativas);
+        }
     }
 
     mostraPergunta();
