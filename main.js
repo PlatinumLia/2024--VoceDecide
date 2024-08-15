49987a4643f6d50db794f6936daf0e03afd364bf
PlatinumLia <134401063+PlatinumLia@users.noreply.github.com>
2024-08-15 08:32:49 -0300

    index, main, style
    Criação de um EventListener

--- main.js
+++ main.js
@@ -7,37 +7,68 @@ const textoResultado = document.querySelector('.texto-resultao');
 const perguntas = [
     {
         enunciado: "texto01",
-        alternativas: [
-            "Alternativa01",
-            "Alternativa02"
+        alternativas: [ 
+            {
+                texto: "alternativa01",
+                afirmacao: "afirmacao01"
+            },
+            {
+                texto: "alternativa02",
+                afirmacao: "afirmacao02"
+            },
         ]    
     },
     {
         enunciado: "Pergunta 2",
         alternativas: [
-            "Alternativa 1",
-            "Alternativa 2"
+            {
+                texto: "Alternativa03",
+                afirmacao: "afirmacao03"
+            },
+            {
+                texto: "Alternativa04",
+                afirmacao: "afirmacao04"
+            },
         ]
     },
     {
         enunciado: "Pergunta 3",
         alternativas: [
-            "Alternativa 1",
-            "Alternativa 2"
+            {
+                texto: "Alternativa05",
+                afirmacao: "afirmacao05"
+            },
+            {
+                texto: "Alternativa06",
+                afirmacao: "afirmacao06"
+            },
+            
         ]
     },
     {
         enunciado: "Pergunta 4",
         alternativas: [
-            "Alternativa 1",
-            "Alternativa 2"
+            {
+                texto: "Alternativa07",
+                afirmacao: "afirmacao07"
+            },
+            {
+                texto: "Alternativa08",
+                afirmacao: "afirmacao08"
+            },
         ]
     },
     {
         enunciado: "Pergunta 5",
         alternativas: [
-            "Alternativa 1",
-            "Alternativa 2"
+            {
+                texto: "Alternativa09",
+                afirmacao: "afirmacao09"
+            },
+            {
+                texto: "Alternativa10",
+                afirmacao: "afirmacao10"
+            }, 
         ]
     }
 ]
@@ -57,7 +88,12 @@ function mostraPergunta()
         for (const alternativa of perguntaAtual.alternativas)
         {
             const botaoAlternativas = document.createElement("button");
-            botaoAlternativas.textContent = alternativa;
+            botaoAlternativas.textContent = alternativa.texto;
+            botaoAlternativas.addEventListener("click", function()
+            {
+                atual++;
+                mostraPergunta();
+            })
             caixaAlternativa.appendChild(botaoAlternativas);
         }
     }
