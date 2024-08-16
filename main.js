3869ba7634bab0fa64ccb15612d59fb7a7049540
PlatinumLia <134401063+PlatinumLia@users.noreply.github.com>
2024-08-16 12:50:16 -0300

    Atualizar o main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const caixaPrincipal = document.querySelector('.caixa-principal');
 const caixaPerguntas = document.querySelector('.caixa-perguntas');
 const caixaAlternativa = document.querySelector('.caixa-alternativa');
 const caixaResultado = document.querySelector('.caixa-resultado');
-const textoResultado = document.querySelector('.texto-resultao');
+const textoResultado = document.querySelector('.texto-resultado');
 
 const perguntas = [
     {
